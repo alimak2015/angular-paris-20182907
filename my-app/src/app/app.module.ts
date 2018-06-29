efe9cde1dd1de92bfe7a95063e86d2adbe6fb6a5
ChristopheGueroult <christophegueroult@gmail.com>
2018-06-29 16:57:26 +0200

    initial commit and generate modules

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { HomeModule } from './home/home.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    HomeModule,
+    PageNotFoundModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
